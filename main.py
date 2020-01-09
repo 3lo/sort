f4e34f6c4ffcd80bd5273bf8310fb1654c05fe68
Eric <41165769+3lo@users.noreply.github.com>
2020-01-09 17:57:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,36 +18,41 @@ class Pillar:
     def __init__(self, area, color):
         self.area = area
         self.color = color
+        self.height = area[3]
 
-    def swap(self):  # Method to update class attributes
-        # pass through 2 class instances, swap their .height attributes. should change that position's height
-        '''
+    @classmethod
+    def bubbleSort(cls, arr):  # Method to update class attributes
         n = len(arr)
         for i in range(n):  # Traverse through all array elements
             for j in range(0, n - i - 1):  # Last i elements are already in place
                 # traverse the array from 0 to n-i-1
                 # Swap if the element found is greater
-                #than the next element
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        return arr
-        '''
-        # need to pygame.draw.rect(screen, white, pygame.Rect(pillar_list[i].area[0], 240 - height, 10, 240))
-        # to draw over and get rid of the previous line
-        pass
+                # than the next element
+                if arr[j].height > arr[j + 1].height:
+                    arr[j].height, arr[j + 1].height = arr[j + 1].height, arr[j].height
+            pygame.draw.rect(screen, black, pygame.Rect(arr[j].area[0], arr[j].area[1], 10, 240))  # clear line
+            pygame.display.update()
+            # pygame.time.wait(500)
+            x, y = arr[j].area[0], 240 - arr[j + 1].height
+            pygame.draw.rect(screen, red, pygame.Rect(x, y, 10, arr[j + 1].height))  # draw new line
 
 
 def background():
     j = 0
     pillar_list = []
+    height_list = []
     for i in range(0, 32):
         height = random.randint(1, 200)
+        height_list.append(height)
         pillar_list.append(Pillar((0 + j, 240 - height, 10, height), red))  # [x, y, width, height]
         pygame.draw.rect(screen, pillar_list[i].color, pygame.Rect(pillar_list[i].area))
         j = j + 10
+    pygame.display.update()
+    pygame.time.wait(1500)
+    print(height_list)
+    print(Pillar.bubbleSort(pillar_list))
 
 
-# pillar_list[i].swap
 background()
 
 while 1:
